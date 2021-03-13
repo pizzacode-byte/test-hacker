2c62279e33f872ca3b79b537badb57e2cd4e873a
pizzacode-byte <piazzacode@gmail.com>
2021-03-13 21:15:25 +0100

    added dropdown menu

---
+++ index.js
@@ -0,0 +1,11 @@
+var widht = window.screen.availWidth;
+console.log(widht);
+
+if(widht < 578)
+    console.log("hey");
+
+else if(widht < 767.98)
+    console.log("hey");
+
+else if(widht < 991.98)
+    console.log("hey");
